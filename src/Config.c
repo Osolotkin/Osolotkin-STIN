145cc9f6a7c9826ae1d2392f7d7f8f7e0e0a9f27
Osolotkin <62661058+Osolotkin@users.noreply.github.com>
2021-04-29 00:19:17 +0200

    Create Config.c

---
+++ src/Config.c
@@ -0,0 +1,18 @@
+#pragma once
+
+#include "Colors.h"
+
+#define SCREEN_WIDTH 800
+#define SCREEN_HEIGHT 640
+
+#define COLOR_MAIN_FRONT WHITE
+#define COLOR_MAIN_BACK BLACK
+
+#define COLOR_SELECTION_BACK HEX_6B9CE2
+#define COLOR_SELECTION_FRONT BLACK
+
+#define COLOR_ERROR 0xEC7063
+
+#define COUNTRIES_TO_SELECT 4
+
+extern const Color graphColors[];
